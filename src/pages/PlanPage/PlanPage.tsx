2e5d21f2bc0b3d5aa889dc0f50f58a986a4b2773
Yuraev Aleksandr <podsolnyx03@mail.ru>
2024-12-05 00:06:43 +0700

    win DnD 2

--- src/pages/PlanPage/PlanPage.tsx
+++ src/pages/PlanPage/PlanPage.tsx
@@ -36,24 +36,9 @@ const PlanPageWrapped = () => {
         })
     );
 
-    function getSemesterIndex(subjectId: UniqueIdentifier): number {
-        return semesters.findIndex(semester =>
-            semester.subjects.find(subject => subject.id === subjectId)
-        );
-    }
-
-    function getSemesterBySubjectId(subjectId: UniqueIdentifier): Semester | undefined {
-        return semesters.find(semester =>
-            semester.subjects.find(subject => subject.id === subjectId)
-        );
-    }
-
-    const isSemesterId = (id: string) => {
-        return id.split("-")[0] === "semesters";
-    }
-
-    const isSelectionId = (id: string) => {
-        return id.split("-")[0] === "selections";
+    function resetAllActiveIds() {
+        setActiveId(null);
+        setActiveOverId(null);
     }
 
     function handleDragStart(event: DragStartEvent) {
@@ -63,37 +48,12 @@ const PlanPageWrapped = () => {
         setActiveId(id);
     }
 
-    function resetAllActiveIds() {
-        setActiveId(null);
-        setActiveOverId(null);
-    }
-
     function handleDragOver(event: DragOverEvent) {
 
         if (!event.over) return;
         const { id: overId } = event.over;
 
         setActiveOverId(overId);
-    }
-
-    //// Handle drag end ////
-
-    //Предмет к предмету в одном семестре
-    //Предмет к предмету в разных семестрах
-    //Предмет из семестра в семестр
-    //Предмет из семестра в выбор
-    //Предмет к предмету в одном выборе
-    //Предмет к предмету в разных выборах
-    //Предмет из выбора в семестр
-
-    const removeSubjectFromSemester = (semesters: Semester[], semesterIndex: number, subjectId: UniqueIdentifier): Semester[] => {
-        return semesters.map((semester, index) =>
-            index !== semesterIndex ? semester :
-                {
-                    ...semester,
-                    subjects: semester.subjects.filter(subject => subject.id !== subjectId)
-                }
-        );
     }
 
     const getParentsIdsByChildId = (id: UniqueIdentifier): UniqueIdentifier[] => {
@@ -142,8 +102,6 @@ const PlanPageWrapped = () => {
 
         const updateSemesters = JSON.parse(JSON.stringify(semesters));
 
-        console.log("ACTIVE", getParentsIdsByChildId(event.active.id), "PASSIVE", getParentsIdsByChildId(event.over.id))
-
         let activeSubject;
 
         const removeSubjectFromParents = (item: any, currentDeep: number) => {
@@ -164,200 +122,25 @@ const PlanPageWrapped = () => {
 
         const addSubjectToNewParents = (item: any, currentDeep: number) => {
             const type = getItemTypeById(parentsIdsOver[currentDeep]);
-            if (!type || currentDeep === parentsIdsOver.length)
+            if (!type)
+                item.subjects.splice(item.subjects.findIndex(_item => _item.id === parentsIdsOver[currentDeep]), 0, activeSubject)
+            else if (currentDeep === parentsIdsOver.length) {
                 item.subjects = [...item.subjects, activeSubject]
+            }
             else {
                 const subItem = item[type].find(_item => _item.id === parentsIdsOver[currentDeep])
                 addSubjectToNewParents(subItem, currentDeep + 1);
             }
         }
 
+        // arrayMove(semester.subjects, activeSubjectIndex, overSubjectIndex)
+
         removeSubjectFromParents({semesters: updateSemesters}, 0)
         addSubjectToNewParents({semesters: updateSemesters}, 0)
 
         setSemesters(JSON.parse(JSON.stringify(updateSemesters)))
-    }
-
-    function handleDragEndSubjectToSemester({active, over}: DragEndEvent) {
-
-        const { id: activeId } = active;
-        const { id: overId } = over;
-
-        const activeSemesterIndex = getSemesterIndex(activeId);
-
-        if (semesters.find(semester => semester.id === overId).subjects.find(subject => subject.id === activeId)) {
-            resetAllActiveIds()
-            return;
-        }
-        else {
-            const activeSubjectIndex = semesters[activeSemesterIndex].subjects.findIndex(subject => subject.id === activeId);
-            const overSemesterIndex = semesters.findIndex(semester => semester.id === overId);
-            let updateSemesters = [...semesters];
-
-            updateSemesters = updateSemesters.map((semester, index) =>
-                index !== activeSemesterIndex ? semester :
-                    {
-                        ...semester,
-                        subjects: semester.subjects.filter((subject, _index) =>
-                            _index !== activeSubjectIndex
-                        )
-                    }
-            );
-
-            updateSemesters = updateSemesters.map((semester, index) => {
-                if (index !== overSemesterIndex) return semester;
-                const subjects = [...semester.subjects];
-                return {
-                    ...semester,
-                    subjects: [...subjects, semesters[activeSemesterIndex].subjects[activeSubjectIndex]]
-                }
-            });
-
-            setSemesters(updateSemesters)
-
-            resetAllActiveIds()
-            return;
-        }
-    }
-
-    function handleDragEndSubjectToSelection({active, over}: DragEndEvent) {
-
-        const { id: activeId } = active;
-        const { id: overId } = over;
-
-        const subjectSemester = getSemesterBySubjectId(activeId)
-
-        if (!subjectSemester) return;
-
-        const subjectSemesterIndex = getSemesterIndex(activeId);
-        const selectionSemesterIndex = semesters.findIndex(semesters => semesters.selections.find(selection => selection.id === overId).id);
-        const activeSubjectIndex = semesters[subjectSemesterIndex].subjects.findIndex(subject => subject.id === activeId);
-
-        if (subjectSemesterIndex === -1 || selectionSemesterIndex === -1) return;
-
-        let updateSemesters = [...semesters];
-        updateSemesters = removeSubjectFromSemester(updateSemesters, subjectSemesterIndex, activeId);
-
-        const key: keyof Semester = "selections";
-
-        updateSemesters = updateSemesters.map((semester, index) => {
-            if (index !== selectionSemesterIndex) return semester;
-            return {
-                ...semester,
-                [key]: (semester[key] && Array.isArray(semester[key])) ? semester[key]?.map(
-                    selection => selection.id !== overId ? selection : {
-                        ...selection,
-                        subjects: [
-                            ...selection.subjects,
-                            {...semesters[subjectSemesterIndex].subjects[activeSubjectIndex]}
-                        ]
-                    }
-                ) : undefined
-            }
-        });
-
-        setSemesters(updateSemesters)
-
-        resetAllActiveIds()
-        return;
-    }
-
-    function handleDragEndSubjectToSubjectSameSemester(event: DragEndEvent, semesterIndex: number) {
-
-        const {active, over} = event;
-
-        const { id: activeId } = active;
-        const { id: overId } = over;
-
-        const subjects = semesters[semesterIndex].subjects;
-
-        const activeSubjectIndex = subjects.findIndex(subject => subject.id === activeId);
-        const overSubjectIndex = subjects.findIndex(subject => subject.id === overId);
-
-        if (activeSubjectIndex !== overSubjectIndex) {
-            setSemesters(semesters.map((semester, index) => index !== semesterIndex ? semester : {
-                ...semester,
-                subjects: arrayMove(semester.subjects, activeSubjectIndex, overSubjectIndex)
-            }))
-        }
-    }
-
-    function handleDragEndSubjectToSubjectAnotherSemester(event: DragEndEvent, activeSemesterIndex: number, overSemesterIndex: number) {
-
-        const {active, over} = event;
-
-        const { id: activeId } = active;
-        const { id: overId } = over;
-
-        const activeSubjects = semesters[activeSemesterIndex].subjects;
-        const overSubjects = semesters[overSemesterIndex].subjects;
-
-        const activeSubjectIndex = activeSubjects.findIndex(subject => subject.id === activeId);
-        const overSubjectIndex = overSubjects.findIndex(subject => subject.id === overId);
-
-        let updateSemesters = [...semesters];
-
-        updateSemesters = updateSemesters.map((semester, index) =>
-            index !== activeSemesterIndex ? semester :
-                {
-                    ...semester,
-                    subjects: semester.subjects.filter((subject, _index) =>
-                        _index !== activeSubjectIndex
-                    )
-                }
-        );
-
-        updateSemesters = updateSemesters.map((semester, index) => {
-            if (index !== overSemesterIndex) return semester;
-            const subjects = [...semester.subjects];
-            subjects.splice(overSubjectIndex, 0, semesters[activeSemesterIndex].subjects[activeSubjectIndex]);
-            return {
-                ...semester,
-                subjects: [...subjects]
-            }
-        });
-
-        setSemesters(updateSemesters)
-    }
-
-    function handleDragEnd(event: DragEndEvent) {
-
-        const {active, over} = event;
-
-        if (!active && !over) return;
-
-        const { id: activeId } = active;
-        const { id: overId } = over;
-
-        if (isSemesterId(overId)) {
-            handleDragEndSubjectToSemester(event)
-            return;
-        }
-
-        if (isSelectionId(overId)) {
-            handleDragEndSubjectToSelection(event)
-            return;
-        }
-
-        const activeSemesterIndex = getSemesterIndex(activeId);
-        const overSemesterIndex = getSemesterIndex(overId);
-
-        if (activeSemesterIndex === -1 || overSemesterIndex === -1) {
-            resetAllActiveIds()
-            return;
-        }
-
-        if (activeSemesterIndex === overSemesterIndex) {
-            handleDragEndSubjectToSubjectSameSemester(event, activeSemesterIndex)
-        }
-        else {
-            handleDragEndSubjectToSubjectAnotherSemester(event, activeSemesterIndex, overSemesterIndex)
-        }
-
         resetAllActiveIds()
     }
-
-    //// ////
 
     return (
         <div className={"flex flex-col bg-stone-100 relative pt-12 h-screen overflow-auto"}>
@@ -392,10 +175,6 @@ const PlanPageWrapped = () => {
                         activeId !== null
                             ? <SubjectCard
                                 id={activeId}
-                                {
-                                    ...semesters[getSemesterIndex(activeId)]?.subjects
-                                        .find(sub => sub.id === activeId)
-                                }
                             />
                             : null
                     }
