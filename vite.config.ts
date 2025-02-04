5fa3e4421e1e88d50c5b48ced31ea6069db9cd05
Yuraev Aleksandr <podsolnyx03@mail.ru>
2025-02-04 21:47:30 +0700

    HOT FIX 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr({include: "**/*.svg?react", svgrOptions: { icon: true } })],
+  plugins: [
+      ImportMetaEnvPlugin.vite({
+        example: '.env.example',
+        env: '.env',
+      }),
+      react(),
+      tsconfigPaths(),
+      svgr({include: "**/*.svg?react", svgrOptions: { icon: true } })
+  ],
   css: {
     preprocessorOptions: {
       scss: {
